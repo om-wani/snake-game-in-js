31e7096e4431de56c78a570e91224379c9f35f1f
omwani <39192098+om-wani@users.noreply.github.com>
2023-03-16 12:52:57 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,162 +1,33 @@
-/*
-
-// creating playground area
-var SizeofBox = 20;// Size of one Block
-var NumberofRows = 20; //total number of rows into the canvas
-var NumberofColumns = 50; //total number of coloumns into the canvas
-var Canvas;
-var Context;
-
-//snake structure
-var SnakeShapeX = SizeofBox * 5;
-var SnakeShapeY = SizeofBox * 5;
-
-//snake speed
-var SnakeSnakeSpeedX = 0;
-var SnakeSnakeSpeedY = 0;
-
-//snake body
-var SnakeBody= [];
-
-//snake food
-var FoodPositionX;
-var FoodPositionY;
-
-//current game status
-var GameOver = false;
-
-window.onload = function (){
-    //settingCanvas height and width
-   Canvas = document.getElementById("canvas");
-   Canvas.height = NumberofRows * SizeofBox;
-   Canvas.width = NumberofColumns * SizeofBox;
-    Context =Canvas.getContext("2d");
-
-    placeFood();
-    document.addEventListener("keyup", changeDirection); //for movements
-    //snake speed
-    setInterval(update, 1000 / 10);
-
-}
-
-function update(){
-    if (GameOver){
-        return;
-    }
-
-    //background of the game 
-    Context.fillStyle = "blue";
-    Context.fillRect(0, 0,Canvas.width,Canvas.height);
-
-    //food color and position
-    Context.fillStyle = "red";
-    Context.fillRect(FoodPositionX, FoodPositionY, SizeofBox, SizeofBox);
-
-    //randomize food
-    if(SnakeShapeX == FoodPositionX && SnakeShapeY == FoodPositionY){
-        SnakeBody.push([FoodPositionX, FoodPositionY]);
-        placeFood();
-    }
-
-    //growing body of snake 
-    for(let i = SnakeBody.length - 1; i > 0; i--){
-        SnakeBody[i] = SnakeBody[i-1];
-    }
-    if(SnakeBody.length){
-        SnakeBody[0] = [SnakeShapeX, SnakeShapeY];
-    }
-
-    Context.fillStyle = "white";
-    SnakeShapeX +=SnakeSnakeSpeedX * SizeofBox;
-    SnakeShapeY += SnakeSnakeSpeedY * SizeofBox;
-    Context.fillRect(SnakeShapeX, SnakeShapeY, SizeofBox, SizeofBox);
-    for(let i = 0; i < SnakeBody.length; i++){
-        Context.fillRect(SnakeBody[i][0], SnakeBody[i][1], SizeofBox, SizeofBox);
-    }
-
-    //game over conditions
-    if(SnakeShapeX < 0 
-        || SnakeShapeX > NumberofColumns * SizeofBox
-        || SnakeShapeY < 0
-        || SnakeShapeY > NumberofRows * SizeofBox){
-            //when snake touch boundry
-            GameOver = true;
-            alert("Game Over");
-    }
-    // when snake touches own body
-    for(let i = 0; i <  SnakeBody.length; i++){
-        if(SnakeShapeX == SnakeBody[i][0] && SnakeShapeY == SnakeBody[i][1]){
-            GameOver = true;
-            alert("Game Over");
-        }
-    }
-}
-
-//movement of snake
-function changeDirection(e){
-    if (e.code == "ArrowUp" && SnakeSnakeSpeedY != -1 || e.code == "W" && SnakeSnakeSpeedY != -1){
-        //when up arrow key pressed snake will move up
-       SnakeSnakeSpeedX = 0;
-        SnakeSnakeSpeedY = -1;
-    }
-    else if (e.code == "ArrowDown" && SnakeSnakeSpeedY != 1 || e.code == "S" && SnakeSnakeSpeedY != 1){
-        //when down arrow pressed snake will move down 
-       SnakeSnakeSpeedX = 0;
-        SnakeSnakeSpeedY = 1;
-    }
-    else if (e.code == "ArrowLeft" &&SnakeSnakeSpeedX != -1 || e.code == "A" &&SnakeSnakeSpeedX != -1){
-        //when left arrow pressed snake will move left 
-       SnakeSnakeSpeedX = -1;
-        SnakeSnakeSpeedY = 0;
-    }
-    else if (e.code == "ArrowRight" &&SnakeSnakeSpeedX != 1 || e.code == "D" &&SnakeSnakeSpeedX != 1){
-        //when right arrow pressed snake will move right 
-       SnakeSnakeSpeedX = 1;
-        SnakeSnakeSpeedY = 0;
-    }
-}
-
-//Randomly place food
-function placeFood(){
-    //x coordinates
-    FoodPositionX = Math.floor(Math.random() * NumberofColumns) * SizeofBox;
-    //y coordinates
-    FoodPositionY = Math.floor(Math.random() * NumberofRows) * SizeofBox;
-}
-
-*/
 
 
 
-// Making playground area 
-var SizeofBox = 20;
-var NumberofRows = 20; //total number of rows 
+
+// Definging the varaibles for making playground area 
+var SizeofBox = 18; //
+var NumberofRows = 30; //total number of rows 
 var NumberofColumns = 50; //total number of coloumns
 var Canvas;
 var Context;
-var Canvas1;
-var Context1;
 
-//snake structure
+//Defining varaibles for making snake's structure.
 var SnakeShapeX = SizeofBox * 5;
 var SnakeShapeY = SizeofBox * 5;
 
-//snake speed
+//Defining varaible for snake speed
 var SnakeSpeedX = 0;
 var SnakeSpeedY = 0;
 
-//snake body
+//Defining the snake's body
 var SnakeBody = [];
 
-//snake food
+// Defining varaibles for the positioning of food
 var FoodPositionX;
 var FoodPositionY;
 
-//current game status
+//The game over status should be false from the start
 var GameOver = false;
 
-
-
+//Stating what should the code do after the window is done loading
 window.onload = function (){
     //setting Canvas height and width
     Canvas = document.getElementById("canvas");
@@ -164,15 +35,9 @@ window.onload = function (){
     Canvas.width = NumberofColumns * SizeofBox;
     Context = Canvas.getContext("2d");
 
-    /*Canvas1 = document.getElementById("border");
-    Canvas1.height = (NumberofRows-1) * SizeofBox;
-    Canvas1.width = (NumberofColumns-1) * SizeofBox;
-    Context1 = Canvas1.getContext("2d");
-*/
-
     placeFood();
-    document.addEventListener("keyup", changeDirection); //for movements
-    //snake speed
+    document.addEventListener("keyup", changeDirection); //key Event listener for the movements
+    //setting the snake speed
     setInterval(update, 1000 / 10);
 
 }
@@ -184,25 +49,21 @@ function update(){
         return;
     }
 
-    //background of the game 
+    //coloring the the play area 
     Context.fillStyle = "#2b3636";
     Context.fillRect(0, 0, Canvas.width, Canvas.height);
 
-    /*//border
-    Context1.fillStyle = "#16e34d";
-    Context1.fillRect(0, 0, Canvas1.width, Canvas1.height);*/
-
-    //food color and position
+    //coloring the food
     Context.fillStyle = "white";
     Context.fillRect(FoodPositionX, FoodPositionY, SizeofBox, SizeofBox);
 
-    //randomize food
+    //to generate new food position
     if(SnakeShapeX == FoodPositionX && SnakeShapeY == FoodPositionY){
         SnakeBody.push([FoodPositionX, FoodPositionY]);
         placeFood();
     }
 
-    //growing body of snake 
+    //to grow and progress the snake's body 
     for(let i = SnakeBody.length - 1; i > 0; i--){
         SnakeBody[i] = SnakeBody[i-1];
     }
@@ -210,7 +71,7 @@ function update(){
         SnakeBody[0] = [SnakeShapeX, SnakeShapeY];
     }
 
-    Context.fillStyle = "red";
+    Context.fillStyle = "red";// coloring the snake 
     SnakeShapeX += SnakeSpeedX * SizeofBox;
     SnakeShapeY += SnakeSpeedY * SizeofBox;
     Context.fillRect(SnakeShapeX, SnakeShapeY, SizeofBox, SizeofBox);
@@ -218,17 +79,17 @@ function update(){
         Context.fillRect(SnakeBody[i][0], SnakeBody[i][1], SizeofBox, SizeofBox);
     }
 
-    //game over conditions
+    //it is game over when the following condition exists
     if(SnakeShapeX < 0 
         || SnakeShapeX > NumberofColumns * SizeofBox
         || SnakeShapeY < 0
         || SnakeShapeY > NumberofRows * SizeofBox){
-            //when snake touch boundry
+            //when snake touches the limits/ boundries of the canvas
             GameOver = true;
             alert("Game Over");
             
     }
-    // when snake touches own body
+    // when snake touches its own body
     for(let i = 0; i <  SnakeBody.length; i++){
         if(SnakeShapeX == SnakeBody[i][0] && SnakeShapeY == SnakeBody[i][1]){
             GameOver = true;
@@ -238,25 +99,25 @@ function update(){
     }
 }
 
-//movement of snake
+//what should happen when you press WASD or arrow keys
 function changeDirection(e){
-    if (e.code == "ArrowUp" && SnakeSpeedY != -1 || e.key === "W" && SnakeSpeedY != -1 || e.key === "w" && SnakeSpeedY != -1){
-        //when up arrow key pressed snake will move up
+    if (e.code == "ArrowUp" && SnakeSpeedY != -1 || e.key === "W" && SnakeSpeedY != -1 || e.key === "w" && SnakeSpeedY != -1 || e.key === "I" && SnakeSpeedY != -1 || e.key === "i" && SnakeSpeedY != -1){
+        //when up arrow key or 'w'pressed snake will move up
         SnakeSpeedX = 0;
         SnakeSpeedY = -1;
     }
-    else if (e.code == "ArrowDown" && SnakeSpeedY != 1 || e.key === "S" && SnakeSpeedY != 1 || e.key === "s" && SnakeSpeedY != 1){
-        //when down arrow pressed snake will move down 
+    else if (e.code == "ArrowDown" && SnakeSpeedY != 1 || e.key === "S" && SnakeSpeedY != 1 || e.key === "s" && SnakeSpeedY != 1 || e.key === "K" && SnakeSpeedY != -1 || e.key === "k" && SnakeSpeedY != -1){
+        //when down arrow or 's' pressed snake will move down 
         SnakeSpeedX = 0;
         SnakeSpeedY = 1;
     }
-    else if (e.code == "ArrowLeft" && SnakeSpeedX != -1 || e.key === "A" && SnakeSpeedX != -1 || e.key === "a" && SnakeSpeedX != -1){
-        //when left arrow pressed snake will move left 
+    else if (e.code == "ArrowLeft" && SnakeSpeedX != -1 || e.key === "A" && SnakeSpeedX != -1 || e.key === "a" && SnakeSpeedX != -1 || e.key === "J" && SnakeSpeedY != -1 || e.key === "j" && SnakeSpeedY != -1){
+        //when left arrow or 'a' pressed snake will move left 
         SnakeSpeedX = -1;
         SnakeSpeedY = 0;
     }
-    else if (e.code == "ArrowRight" && SnakeSpeedX != 1 || e.key === "D" && SnakeSpeedX != 1 || e.key === "d" && SnakeSpeedX != 1){
-        //when right arrow pressed snake will move right 
+    else if (e.code == "ArrowRight" && SnakeSpeedX != 1 || e.key === "D" && SnakeSpeedX != 1 || e.key === "d" && SnakeSpeedX != 1 || e.key === "L" && SnakeSpeedY != -1 || e.key === "l" && SnakeSpeedY != -1){
+        //when right arrow or 'd' pressed snake will move right 
         SnakeSpeedX = 1;
         SnakeSpeedY = 0;
     }
